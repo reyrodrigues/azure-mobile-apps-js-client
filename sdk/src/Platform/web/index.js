e4531269b7f6101c472fed16f242f5f6050f063d
ruslan-bikkinin <v-rubikk@microsoft.com>
2017-11-21 16:41:48 +0300

    Fix OAuth requests rejection for Google provider
    Google no longer supports InAppBrowser plugin to perform OAuth requests
    within it. To fix it InAppBrowser was replaced by native calls to
    MobileService.login which uses reliable azure-mobile-apps-android-client
    implementation of login.

--- sdk/src/Platform/web/index.js
+++ sdk/src/Platform/web/index.js
@@ -148,7 +148,7 @@ exports.getSdkInfo = function () {
     };
 };
 
-exports.login = function (startUri, endUri, callback) {
+exports.login = function (startUri, endUri, callback, providerName, appUrl) {
     // Force logins to go over HTTPS because the runtime is hardcoded to redirect
     // the server flow back to HTTPS, and we need the origin to match.
     var findProtocol = /^[a-z]+:/,
@@ -158,7 +158,7 @@ exports.login = function (startUri, endUri, callback) {
         endUri = endUri.replace(findProtocol, requiredProtocol);
     }
 
-    return getBestProvider(knownLoginUis).login(startUri, endUri, callback);
+    return getBestProvider(knownLoginUis).login(startUri, endUri, callback, providerName, appUrl);
 };
 
 exports.toJson = function (value) {
